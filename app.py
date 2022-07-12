956911b20388ba3fb27fe1fd6f156389478f8b70
workruixuan <109124904+workruixuan@users.noreply.github.com>
2022-07-12 16:43:32 +0800

    change the entire project

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect, url_for, render_template, Response, request
+from flask import Flask, render_template, Response
 from camera import Video
-app = Flask(__name__)
 
+app=Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
 
 def gen(camera):
     while True:
@@ -14,27 +14,10 @@ def gen(camera):
        b'Content-Type:  image/jpeg\r\n\r\n' + frame +
          b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
+@app.route('/video')
+
+def video():
     return Response(gen(Video()),
     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# @app.route('/success/<name>')
-# def success(name):
-#    return 'welcome1 %s' % name
-
-# @app.route('/success2/<name>')
-# def success2(name):
-#    return 'welcome sot %s' % name
-
-# @app.route('/login',methods = ['POST', 'GET'])
-# def login():
-#    if request.method == 'POST':
-#       user = request.form['nm']
-#       return redirect(url_for('success',name = user))
-#    else:
-#       user = request.args.get('nm')
-#       return redirect(url_for('success2',name = user))
-
-if __name__ == '__main__':
-   app.run(debug = True)
+app.run(debug=True)
