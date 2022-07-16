c8d3e547acb8624a5f7e0a3186eec171e113af48
workruixuan <109124904+workruixuan@users.noreply.github.com>
2022-07-16 16:36:21 +0800

    h

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-   app.run(port=81)
+    # app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=81)
