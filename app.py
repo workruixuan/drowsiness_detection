7c9777ba100c17e20265bd8196aebd12d864406a
workruixuan <109124904+workruixuan@users.noreply.github.com>
2022-07-21 14:39:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ def gen(camera):
        b'Content-Type:  image/jpeg\r\n\r\n' + frame +
          b'\r\n\r\n')
 
-@app.route('/video')
 
+@app.route('/video')
 def video():
     return Response(gen(Video()),
-    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
